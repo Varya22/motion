ebd94939db1ece8c86d6c94fd31cf2b2b646d926
Varya22 <100872744+Varya22@users.noreply.github.com>
2023-04-28 23:56:20 +0300

    motion

---
+++ fitness/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        port: '5432',
+        user: 'postgres',
+        password: '1234',
+        database: 'fit'
+    }
+}
